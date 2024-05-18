a70453ca2e1cdd49903b8ab797fa7d3143a8d71b
thangarajan1 <saaralk.t.rajan2@gmail.com>
2024-05-18 09:36:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const app = express();
+const cors = require("cors")
+const port = 8000;
+const fs = require("fs")
+const courseData = require("../src/CardData.json")
+const reviewData = require("../src/ReviewCard.json")
+
+app.use(express.json())
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000","http://localhost:8000"],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  })
+);
+
+app.post("/review", (req, res) => {
+  let {img, name, coursename, review } = req.body;
+  if (!img ||!name || !coursename || !review) {
+    res.status(400).send({ message: "All fields Required" });
+  }
+  let id = Date.now();
+  reviewData.push({id,img, name, coursename, review });
+  fs.writeFile(
+    "../src/ReviewCard.json",
+    JSON.stringify(reviewData),
+    (err, data) => {
+      return res.json({ message: "User Details ADD" });
+    }
+  );
+});
+
+app.delete("/review/:id",(req,res)=>{
+  let id = Number(req.params.id);
+  let filter = reviewData.filter((user)=>user.id !== id);
+  fs.writeFile(
+    "../src/ReviewCard.json",
+    JSON.stringify(filter),
+    (err, data) => {
+      return res.json(filter);
+    }
+  );
+  console.log("Deleted");
+})
+app.patch("/review/:id", (req, res) => {
+  let id = Number(req.params.id);
+let { img,name, coursename, review } = req.body;
+if (!img ||!name || !coursename || !review) {
+  res.status(400).send({ message: "All fields Required" });
+}
+let index =reviewData.findIndex((user)=> user.id == id);
+reviewData.splice(index,1,{...req.body})
+  fs.writeFile(
+    "../src/ReviewCard.json",
+    JSON.stringify(reviewData),
+    (err, data) => {
+      return res.json({ message: "User Details Updated" });
+
+    }
+  );
+});
+
+
+app.get("/course",(req,res)=>{
+    return res.json(courseData);
+})
+
+app.get("/review", (req, res) => {
+  return res.json(reviewData);
+});
+
+app.listen(port,(err)=>{
+    console.log(`app is running in ${port}`);
+})
