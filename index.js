beab0b1daf423279a781c38527365ed7d4092a26
thangarajan1 <163718553+thangarajan1@users.noreply.github.com>
2024-05-18 17:34:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: ["http://localhost:3000","http://localhost:8000"],
+    origin: ["http://localhost:3000","http://localhost:8000","https://frondcadd.onrender.com"],
     methods: ["GET", "POST", "PATCH", "DELETE"],
   })
 );
